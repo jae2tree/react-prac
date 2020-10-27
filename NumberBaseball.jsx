6610c3ea9293cc4524be1b7c2b4a5bc4a8611c62
jae2tree <jae2tree@gmail.com>
2020-10-27 17:07:51 +0900

    prevState 수정

--- NumberBaseball.jsx
+++ NumberBaseball.jsx
@@ -22,9 +22,11 @@ class NumberBaseball extends Component {
   onSubmitForm = (e) => {
     e.preventDefault();
     if (this.state.value === this.state.answer.join('')) {
-      this.setState({
-        result: '홈런!',
-        tries: [...this.state.tries, { try: this.state.value, result: '홈런!'}],
+      this.setState((prevState) => {
+        return {
+          result: '홈런!',
+          tries: [...this.state.tries, { try: this.state.value, result: '홈런!'}],
+        };
       });
       alert('게임을 다시 시작합니다!');
       this.setState({
@@ -54,9 +56,11 @@ class NumberBaseball extends Component {
             ball += 1;
           }
         }
-        this.setState({
-          tries: [...this.state.tries, { try: this.state.value, result: `${strike}스트라이크, ${ball}볼 입니다.`}],
-          value: '',
+        this.setState((prevState) => {
+          return {
+            tries: [...this.state.tries, { try: this.state.value, result: `${strike}스트라이크, ${ball}볼 입니다.`}],
+            value: '',
+          }
         });
       }
     }
