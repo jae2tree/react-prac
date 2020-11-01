779d48708f80cbff3cae94d640725d326111ad5f
jae2tree <jae2tree@gmail.com>
2020-11-01 20:00:45 +0900

    가위바위보 게임

--- client.jsx
+++ client.jsx
@@ -8,7 +8,10 @@ import { hot } from 'react-hot-loader/root';
 // import NumberBaseball from './NumberBaseball';
 // const Hot = hot(NumberBaseball);
 
-import ResponseCheck from './ResponseCheck';
-const Hot = hot(ResponseCheck);
+//import ResponseCheck from './ResponseCheck';
+//const Hot = hot(ResponseCheck);
+
+import RSP from './RSP';
+const Hot = hot(RSP);
 
 ReactDom.render(<Hot />, document.querySelector('#root'));
