022bf36994ed58207046a3a04ae8f3aa764841a3
jae2tree <jae2tree@gmail.com>
2020-11-12 20:06:36 +0900

    useMemo, useCallback

--- Lotto.jsx
+++ Lotto.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
 import Ball from './Ball';
 
 function getWinNumbers() {
@@ -14,7 +14,8 @@ function getWinNumbers() {
 }
 
 const Lotto = () => {
-  const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+  const lottoNumbers = useMemo(() => getWinNumbers(), []);
+  const [winNumbers, setWinNumbers] = useState(lottoNumbers);
   const [winBalls, setWinBalls] = useState([]);
   const [bonus, setBonus] = useState(null);
   const [redo, setRedo] = useState(false);
@@ -38,14 +39,23 @@ const Lotto = () => {
     }
   } , [timeouts.current]);
 
-  const onClickRedo = () => {
+  const onClickRedo = useCallback(() => {
+    console.log(winNumbers);
     setWinNumbers(getWinNumbers());
     setWinBalls([]);
     setBonus(null);
     setRedo(false);
 
     timeouts.current = [];
-  };
+  }, [winNumbers]);
+  // useCallback은 함수를 다시 선언? 해주는 문제를 해결하기 위해 있는데
+  // useCalback으로 감싼 함수를 기억해준다.
+  // 하지만 useCallback에서 state를 사용시 그변하기 전의 값까지 계속 기억해 버려서
+  // 두번째 인자의 배열에 사용하는 state의 값을 넣어줘야 한다.
+
+  // 주로 자식컴퍼넌트에 props로 넘겨줄 함수를 useCallback으로 감싼다
+
+  // useMemo도 마찬가지로 함수를 다시 선언해주지 않고 기억시키기 위해 사용한다.
 
   return (
     <>
