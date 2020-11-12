0411e37222ca3396b742e369f4932e27ebab95bd
jae2tree <jae2tree@gmail.com>
2020-11-12 19:17:10 +0900

    Lotto

--- client.jsx
+++ client.jsx
@@ -11,7 +11,10 @@ import { hot } from 'react-hot-loader/root';
 //import ResponseCheck from './ResponseCheck';
 //const Hot = hot(ResponseCheck);
 
-import RSP from './RSP';
-const Hot = hot(RSP);
+//import RSP from './RSP';
+//const Hot = hot(RSP);
+
+import Lotto from './Lotto';
+const Hot = Lotto;
 
 ReactDom.render(<Hot />, document.querySelector('#root'));
